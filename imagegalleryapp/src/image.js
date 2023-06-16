bdc89e79bd65abc7cc11131bfa69b158803698ec
Bobrekk <81027034+Bobrekk@users.noreply.github.com>
2023-06-16 23:33:29 +0300

    Add files via upload

---
+++ imagegalleryapp/src/image.js
@@ -0,0 +1,4 @@
+export default [
+    "https://siradisidigital.com/upload/yemek-fotografi-nasil-cekilir-ankara-dijital-ajans-siradisi-digital-5.jpg",
+    "https://i.cnnturk.com/i/cnnturk/75/0x555/57b1ac2561361f1dd4e751fc.jpg"
+]
